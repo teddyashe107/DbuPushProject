7352d40583a3250b41e180c45b33cf611aa02343
teddyashe17 <teddyashe107@gmail.com>
2021-06-02 20:53:23 +0300

    creating Api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('user', [ApiController::class]);
+
+//Route::get('data',  [ApiController::class, 'index']);
+//Route::post('store',[ApiController::class, 'store']);
